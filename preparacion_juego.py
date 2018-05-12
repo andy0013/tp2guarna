3ff46becbdbb0692b5463b86fe7682d2bcfbf355
parias <ariaspablo@gmail.com>
2018-05-12 14:30:06 -0300

    se le hizo algunas modificaciones al archivo de andy

--- preparacion_juego.py
+++ preparacion_juego.py
@@ -6,10 +6,10 @@ def obtener_numero(mensaje):
     while not numero.isdigit():
         print("ingrese numero no letras")
         numero = input(mensaje)
-    return numero
+    return int(numero)
 
 
-def obtener_palabra(mensaje):
+def obtener_string(mensaje):
     palabra = input(mensaje)
     while palabra.isdigit():
         print("no ingresar numeros")
@@ -22,18 +22,18 @@ def ingresar_nombre_jugadores():
     listaJugadores = []
 
     while seguir:
-        cantidadJugadores = int(obtener_numero("Ingrese el numero de jugadores: "))
+        cantidadJugadores = obtener_numero("Ingrese el numero de jugadores: ")
         if cantidadJugadores <= 10:
             seguir = False
         else:
             print("La cantidad de jugadores permitida debe ser menor a 10 , ingrese nuevamente un valor adecuado")
 
     for i in range(cantidadJugadores):
-        nombreJugador = obtener_palabra("Ingrese el nombre completo de los jugadores:")
+        nombreJugador = obtener_string("Ingrese el nombre completo de los jugadores:")
         listaJugadores.append(nombreJugador)
     return listaJugadores
 
-def obtener_lista_jugadores(listaJugadores):
+def imprimir_lista_jugadores(listaJugadores):
     "esta funcion recibe la lista de jugadores creada anteriormente y va a imprimir\
     una lista random de jugadores para que los usuarios sepan el orden de juego\
     y va a devolver la lista random "
@@ -43,57 +43,67 @@ def obtener_lista_jugadores(listaJugadores):
     for index in range(len(listaJugadores)):
         posicion=index+1
         print(posicion , listaJugadores[index])
-    return listaJugadores
 
-def cantidaddeletras(mensaje,mensaje2):
+
+def obtener_cantidad_de_letras_en_palabra():
     "funcion que pide a los jugadores la longitud de la palabra a adivinar , corrobora que sea mayorigual a 5\
     una vez que tiene el valor, devuelve el valor de la longitud"
     seguir = True
     while seguir:
-        letrasenpalabra = int(input(mensaje))
-        if letrasenpalabra >= 5:
+        letrasEnPalabra = obtener_numero("Ingrese la cantidad de letras en la palabra: ")
+        if letrasEnPalabra > 5:
             seguir = False
         else:
-            print(mensaje2)
-    return letrasenpalabra
+            print("La cantidad de letras en la palabra a adivinar debe ser mayor que 5")
+    return letrasEnPalabra
 
-def obtiene_lista_palabras_a_adivinar(lista):
+def crear_diccionario_palabras_longitud(listaPalabrasPotenciales):
 
-def darlistadepalabrasentextoconletrapedida(lista_palabras_en_texto,numero_de_letras):
-    "esta funcion recibe la lista de de palabras con su valor de longitud de letras\
-    que fue hecha en la funcion (lista_de_las_palabras_con_su_longitud) y recibe\
-    el valor que contiene la cantidad de letras a adivinar que devuelve la funcion\
-    (cantidaddeletras), con esos valores busca la palabra que tenga la cantidad de letras pedida,\
-    finalmente devuelve una lista con las palabras que tienen la longitud ingresada"
+    diccionarioLongitudPalabras={}
 
-    palabras_en_el_texto = []
-    for palabras in lista_palabras_en_texto:
-        if palabras[1] == numero_de_letras:
-            palabras_en_el_texto += [palabras[0]]
-    return palabras_en_el_texto
+    for palabra in listaPalabrasPotenciales:
+        key=len(palabra)
 
+        if key not in diccionarioLongitudPalabras.keys():
+            diccionarioLongitudPalabras[key]=[]
 
-def asignarpalabraacadajugador(lista_de_nombres_random,listadepalasconletra):
-    "esta funcion deberia devolver lo que vos pedis Pablo"
-    jugadorysupalabra = {}
-    random.shuffle(listadepalasconletra,random.random)
-    cantidaduno = len(lista_de_nombres_random)
-    for numeros in range(cantidaduno):
-        jugadorysupalabra[lista_de_nombres_random[numeros]] = [listadepalasconletra[numeros],0,0,0]
-    return jugadorysupalabra
+        diccionarioLongitudPalabras[key].append(palabra)
 
-def Pablo():
-    nombres = usuarios_nombres("ingrese la cantidad de jugadores: ","ingrese el nombre de el jugador: ")
-    turnos = l_random(nombres)
-    letra = cantidaddeletras("ingrese cantidad de letras: ","ingrese solo un numero mayor a 5")
-    lista_longitud = lista_de_las_palabras_con_su_longitud(PalabrasValidas)
-    lista_hecha = darlistadepalabrasentextoconletrapedida(lista_longitud,letra)
-    l_final = asignarpalabraacadajugador(turnos,lista_hecha)
-    return l_final
+    return diccionarioLongitudPalabras
 
 
-l_oraciones = texto.obtener_texto()
-PalabrasEnTexto = Palabras(l_oraciones)
-PalabrasValidas= EliminarRepes(PalabrasEnTexto)
-Dicc = CrearDicc(PalabrasEnTexto)
-print(Pablo())
+def obtiene_lista_palabras_a_adivinar(diccionarioLongitudPalabras,cantidadJugadores):
+    """Esta funcion pide que se ingrese el tamaño de la palabra a adivinar y si para esa
+    cantidad no hay suficientes palabras pide que se ingrese un nuevo valor.
+    devuelve la lista random de palabras de ese tamaño"""
+
+    flag=True
+    listaPalabrasAAdivinar=[]
+
+    while flag:
+        longitudPalabra=obtener_cantidad_de_letras_en_palabra()
+        if (len(diccionarioLongitudPalabras[longitudPalabra]) >= cantidadJugadores):
+            flag=False
+        else:
+            print("No hay suficientes palabras de la longitud {0} para la cantidad de jugadores {1}. Vuelva a ingresar otro tamaño de palabra".format(longitudPalabra,cantidadJugadores))
+
+    for i in range(cantidadJugadores):
+        listaPalabrasAAdivinar.append(diccionarioLongitudPalabras[longitudPalabra][i])
+
+    return random.shuffle(listaPalabrasAAdivinar)
+
+
+def preparacion_juego():
+
+    jugadores={}
+
+    listaDePalabrasPotenciales=precondiciones.precondiciones()
+
+    listaDeJugadores=ingresar_nombre_jugadores()
+    imprimir_lista_jugadores(listaDeJugadores)
+    diccionarioLongitudPalabras=crear_diccionario_palabras_longitud(listaDePalabrasPotenciales)
+    listaDePalabrasAAdivinar=obtiene_lista_palabras_a_adivinar(diccionarioLongitudPalabras,len(listaDeJugadores))
+
+    i=0
+    for jugador in listaDeJugadores
+        jug

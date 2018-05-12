44771eaf71cb7ad931afe205bae5ad31ec719d4d
parias <ariaspablo@gmail.com>
2018-05-12 00:47:56 -0300

    se hicieron algunos ajustes a lo que hizo leandro

--- precondiciones.py
+++ precondiciones.py
@@ -3,33 +3,38 @@ import texto
 import unicodedata
 
 def elimina_tildes(s):
-   return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+    """Elimina las tildes de un string. Realizada por Leandro"""
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
 
 def obtiene_lista_palabras(oraciones):
+    """Obtiene una lista de palabras del texto suministrado por la catedra. Realizada por Leandro"""
     listaf = []
     for i in oraciones :
         lista = re.split("[(, \-!?:)\"\.;><1234567890]+" , i.upper())
         listaf.extend(lista)
     return sorted(listaf)
 
-def eliminar_repetidas(texto):
+def eliminar_repetidas(lista):
+    """Elimina las palabras repetidas. Realizada por Leandro"""
     final = []
-    for i in texto:
-        if (len(i) >= 5):
-            if not i in final:
-                final.append(elimina_tildes(i))
+    for i in lista:
+        if not i in final:
+            final.append(elimina_tildes(i))
     return sorted(final)
 
 def crear_diccionario(validas, palabras):
-    dicc = {}
+    """Crea un diccionario a partir de la lista de palbras y la frecuenccia de dichas palabras. Realizada por Leandro."""
+    diccionario = {}
     for n in validas:
-        dicc[n] = 0
+        diccionario[n] = 0
     for i in palabras:
-        if i in dicc:
-            dicc[i] += 1
-    return dicc
+        if i in diccionario:
+            diccionario[i] += 1
+    del diccionario['']
+    return diccionario
 
 def precondiciones():
+    """Devuelve una lista de palabras en mayusculas y sin acentos. Realizada por Leandro"""
     oraciones = texto.obtener_texto()
     palabrasEnTexto = obtiene_lista_palabras(oraciones)
     palabrasValidas = eliminar_repetidas(palabrasEnTexto)
@@ -40,6 +45,6 @@ def precondiciones():
     print(diccionario)
     print("#################################################################################")
     print("Cantidad de palabras en el diccionario: ", len(diccionario))
-    return(diccionario)
+    return(list(diccionario.keys()))
 
-precondiciones()
+print(precondiciones())
